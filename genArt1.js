a5cc036e523d565b8d40655fb6745eee084ada6b
Luke Penner <lukepenner@hotmail.ca>
2021-04-04 14:47:18 -0700

    particles follow mouse

--- genArt1.js
+++ genArt1.js
@@ -18,19 +18,25 @@ const mouse = {
 canvas.addEventListener('click', function(e){
   mouse.x = e.x;
   mouse.y = e.y
+  for (let i = 0; i  < 10; i++){
+    particleArray.push(new Particle());
+  }
 })
 
 canvas.addEventListener('mousemove', function(e) {
   mouse.x = e.x;
-  mouse.y = e.y
+  mouse.y = e.y;
+  for (let i = 0; i  < 10; i++){
+    particleArray.push(new Particle());
+  }
 })
 
 class Particle {
   constructor() {
-    //this.x = mouse.x;
-    //this.y = mouse.y;
-    this.x = Math.random() * canvas.width
-    this.y = Math.random() * canvas.height
+    this.x = mouse.x;
+    this.y = mouse.y;
+    //this.x = Math.random() * canvas.width
+    //this.y = Math.random() * canvas.height
     this.size = Math.random() * 5 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
@@ -39,6 +45,7 @@ class Particle {
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.1;
   }
   draw() {
     ctx.fillStyle = '#FFFFFF';
@@ -60,6 +67,9 @@ const handleParticles = function() {
   for (let i = 0; i < particleArray.length; i++) {
     particleArray[i].update();
     particleArray[i].draw();
+    if (particleArray[i].size <= 0.3) {
+      particleArray.splice(i,1);
+    }
   }
 }
 
