aa44ecd4b7b7cde9f414a965eb3e29fe0bd551cc
Luke Penner <lukepenner@hotmail.ca>
2021-04-04 15:23:18 -0700

    touch functionality??

--- genArt1.js
+++ genArt1.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const particleArray = [];
+const ongoingTouches = [];
 
 window.addEventListener('resize', function() {
   canvas.width = window.innerWidth;
@@ -17,13 +18,20 @@ const mouse = {
 
 canvas.addEventListener('click', function(e){
   mouse.x = e.x;
-  mouse.y = e.y
+  mouse.y = e.y;
   for (let i = 0; i  < 10; i++){
     particleArray.push(new Particle());
   }
 })
 
 canvas.addEventListener('mousemove', function(e) {
+  mouse.x = e.x;
+  mouse.y = e.y;
+  for (let i = 0; i  < 10; i++){
+    particleArray.push(new Particle());
+  }
+})
+canvas.addEventListener('touch', function(e) {
   mouse.x = e.x;
   mouse.y = e.y;
   for (let i = 0; i  < 10; i++){
@@ -37,7 +45,7 @@ class Particle {
     this.y = mouse.y;
     //this.x = Math.random() * canvas.width
     //this.y = Math.random() * canvas.height
-    this.size = Math.random() * 5 + 1;
+    this.size = Math.random() * 3 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
 
@@ -45,7 +53,7 @@ class Particle {
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
-    if (this.size > 0.2) this.size -= 0.1;
+    if (this.size > 0.2) this.size -= 0.05;
   }
   draw() {
     ctx.fillStyle = '#FFFFFF';
@@ -74,10 +82,92 @@ const handleParticles = function() {
 }
 
 const animate = function() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+  //ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.fillStyle = 'rgba(0,0,0,0.1)';
+  ctx.fillRect(0,0,canvas.width,canvas.height);
   handleParticles();
   requestAnimationFrame(animate);
 }
 
+
+
+//--------------------------------------------------------------
+//touch handling
+//--------------------------------------------------------------
+function startup() {
+  canvas.addEventListener("touchstart", handleStart, false);
+  canvas.addEventListener("touchend", handleEnd, false);
+  canvas.addEventListener("touchcancel", handleCancel, false);
+  canvas.addEventListener("touchmove", handleMove, false);
+}
+
+document.addEventListener("DOMContentLoaded", startup);
+
+function handleStart(evt) {
+  evt.preventDefault();
+  console.log("touchstart.");
+  var touches = evt.changedTouches;
+
+  for (var i = 0; i < touches.length; i++) {
+    console.log("touchstart:" + i + "...");
+    ongoingTouches.push(copyTouch(touches[i]));
+    mouse.x = touches[i].pageX;
+    mouse.y = touches[i].pageY;
+    for (let i = 0; i  < 10; i++){
+      particleArray.push(new Particle());
+    }
+    console.log("touchstart:" + i + ".");
+  }
+}
+function handleMove(evt) {
+  evt.preventDefault();
+  var touches = evt.changedTouches;
+
+  for (var i = 0; i < touches.length; i++) {
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+
+    if (idx >= 0) {
+      console.log("continuing touch "+idx);
+
+      mouse.x = touches[i].pageX;
+      mouse.y = touches[i].pageY;
+      for (let i = 0; i  < 10; i++){
+        particleArray.push(new Particle());
+      }
+
+      ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
+      console.log(".");
+    } else {
+      console.log("can't figure out which touch to continue");
+    }
+  }
+}
+function handleEnd(evt) {
+  evt.preventDefault();
+  log("touchend");
+  var touches = evt.changedTouches;
+
+  for (var i = 0; i < touches.length; i++) {
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+
+    if (idx >= 0) {
+      ongoingTouches.splice(idx, 1);  // remove it; we're done
+    } else {
+      console.log("can't figure out which touch to end");
+    }
+  }
+}
+function handleCancel(evt) {
+  evt.preventDefault();
+  console.log("touchcancel.");
+  var touches = evt.changedTouches;
+
+  for (var i = 0; i < touches.length; i++) {
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+    ongoingTouches.splice(idx, 1);  // remove it; we're done
+  }
+}
+
+//-------------------------------------------------------------
 
 animate();
